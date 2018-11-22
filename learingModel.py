c0ac91cc577d4230037c3a27883c5053097aa152
isseikz <kuzumaki-issei-sk@ynu.jp>
2018-11-22 23:40:15 +0900

    making learning data

--- learingModel.py
+++ learingModel.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import csv, os
+import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 from pytz import timezone
+import re
+
+import visualizer as v
 
 # TODO: リストアップ
 def snapshot_list():
@@ -17,12 +21,18 @@ def snapshots_10(l, n):
     # print(s)
     return s
 
+# TODO: n 番目から n+ q番目までのスナップショットのリストを取得
+def snapshots_between(l, first, last):
+    s = l[first : last]
+    return np.array(s)
+
 def str2float(str_list):
     return list(map(float, str_list))
 
 # TODO: 10個のスナップショットから平面を作成
 def snapshots_traj(l):
-    traj = np.zeros((10,600,2), dtype=float)
+    s = l.shape
+    traj = np.zeros((s[0],600,2), dtype=float)
     for i, snapshot_path in enumerate(l):
         with open(snapshot_path) as file:
             reader = csv.reader(file)
@@ -49,10 +59,183 @@ def nth_snapshot(l,n):
         # print(sf)
     return sf
 
-def makeModelData(quantity):
+# TODO: 学習モデルの大きさ定義
+def modelArr(foldername):
+    thisDir = os.path.dirname(os.path.abspath(__file__))
+    print(thisDir)
+
+    path = './model/' + foldername + '/'
+
+    modelName = ["train","test"]
+    labels     = ["should_buy","should_sell","both","neither"]
+
+    modelSize = np.zeros((len(modelName),len(labels)),dtype=int)
+
+    pathLabel = modelName
+    pathModel = modelName
+    # todo: listModelの大きさを調べて，空行列を設ける
+    # TODO:
+
+    for id, name in enumerate(modelName):
+        print(name)
+        pathLabel[id] = list(Path(path + name).glob(".csv"))
+
+        for j, label in enumerate(labels):
+            pathModel = Path( path + name + "/" + label + "/")
+            print(pathModel)
+            listModel = list(pathModel.glob("*.csv"))
+            modelSize[id,j] = len(listModel)
+
+    print(modelSize)
+    return listModel, modelName, labels, modelSize
+
+'''
+    既存の学習モデルを読み出す
+    入力：モデル名
+    出力：訓練データ・テストデータ・それぞれのラベル
+'''
+def loadModel(foldername, nSnapshots):
+    listModel, modelName, labels, modelSize = modelArr(foldername)
+    print(listModel, modelName, labels, modelSize)
+
+    dataSize_train = modelSize[0,:].sum()
+    dataSize_test  = modelSize[1,:].sum()
+    allModels = np.zeros((nSnapshots,600,2), dtype=float)
+    for id, name in enumerate(modelName):
+        for j, label in enumerate(labels):
+            for k, modelData in enumerate(listModel):
+                with open(modelData) as file:
+                    reader = csv.reader(file)
+                    data = [row for row in list(reader) if row != []]
+
+                    model = np.zeros((nSnapshots,600,2), dtype=float)
+                    for l, order in enumerate(data):
+                        arr = np.array(order)
+                        for m, strValue in enumerate(arr):
+                            # print(strValue)
+                            regx1 = r'(\d+\.\d+e[\+|\-]\d+)+'
+                            regx2 = r'(\d+)+'
+                            # regx1 = r'[(\d+\.\d+e[\+|\-]\d+)+]|[\d+\.]'
+                            valsize = re.findall(regx1, strValue)
+
+                            if len(valsize) == 0:
+                                valsize = re.findall(regx2, strValue)
+                                # print(valsize)
+
+                            s = np.array(valsize)
+                            sf = s.astype(np.float)
+                            # print(sf)
+                            model[l,m,:] = sf
+
+                    models[k] = model
+                    print(models)
+                    allModels.append(models)
+    print(allModels)
+    return allModels, dataSize_train, dataSize_test
+
+
+'''
+    リファレンスファイルからパスを読み取り，モデルにする
+    入力: foldername: モデル用データの入っているフォルダ
+    出力: datasArr_test, labelArr_test, datasArr_train, labelArr_train: リファレンスから得られるデータとラベル
+'''
+def loadDatas(foldername):
+    dataType = ["test","train"]
+    regx1 = r'(\d+\.\d+e[\+|\-]\d+)+'
+    regx2 = r'(\d+)+'
+
+    path_model = './model/' + foldername + '/'
+
+    path_modelDir = [""] * 2
+    path_refFile = [""] * 2
+
+    for id in range(2):
+        path_modelDir[id] = path_model + dataType[id] + "/"
+        path_refFile[id]  = path_modelDir[id]  + "reference.csv"
+
+    dataSize = [0] * 2 # テスト(0), 学習(1)データの要素数
+
+    datasArr_test  = []
+    datasArr_train = []
+
+    labelArr_test  = []
+    labelArr_train = []
+
+    # datas = np.ndarray()
+    for i,path in enumerate(path_refFile):
+        with open(Path(path)) as refFile:
+            reader = csv.reader(refFile)
+            dataList = np.array([row for row in list(reader) if row != []])
+            dataSize[i] = len(dataList)
+            print(dataSize[i])
+
+            # 各データを読み出して，学習用データにする
+            # for j,data in enumerate(dataList):
+            #     if j % 100 == 0:
+            #         print(f'{dataType[i]}, {(j+100)/dataSize[i]*100}%')
+            #
+            #     snapshots = pd.read_csv(data[1], header=None).values
+            #
+            #     should_buy  = 1 if data[2] == "True" else 0
+            #     should_sell = 1 if data[3] == "True" else 0
+            #     if i == 0:
+            #         datasArr_test.append(snapshots)
+            #         labelArr_test.append(int(should_buy*2+should_sell))
+            #     else:
+            #         datasArr_train.append(snapshots)
+            #         labelArr_train.append(int(should_buy*2+should_sell))
+
+            for j,data in enumerate(dataList):
+                if j % 100 == 0:
+                    print(f'{dataType[i]}, {(j+100)/dataSize[i]*100}%')
+
+                with open(Path(data[1])) as pathData:
+                    csvData = csv.reader(pathData)
+                    strSnapshots = np.array([row for row in list(csvData) if row != []])
+                    # print(strSnapshots.shape)
+
+                    snapshots = np.ndarray((strSnapshots.shape[0],600,2),dtype=float)
+                    for l,strSnapshot in enumerate(strSnapshots):
+                        # print(len(strSnapshot))
+                        snapshot = np.ndarray((600,2),dtype=float)
+                        for k,strValue in enumerate(strSnapshot):
+                            valsize = re.findall(regx1, strValue)
+
+                            if len(valsize) == 0:
+                                valsize = re.findall(regx2, strValue)
+                                # print(valsize)
+
+                            s = np.array(valsize)
+                            sf = s.astype(np.float)
+                            snapshot[k,:] = sf
+
+                        snapshots[l,:,:] = snapshot
+
+                    should_buy  = 1 if data[2] == "True" else 0
+                    should_sell = 1 if data[3] == "True" else 0
+                    if i == 0:
+                        datasArr_test.append(snapshots)
+                        labelArr_test.append(int(should_buy*2+should_sell))
+                    else:
+                        datasArr_train.append(snapshots)
+                        labelArr_train.append(int(should_buy*2+should_sell))
+
+    print(datasArr_train)
+    print(labelArr_train)
+    return datasArr_test, labelArr_test, datasArr_train, labelArr_train
+
+'''
+    # データから学習用データを作る
+    ## 入力
+    + datasSise: 1つのデータに用いる気配値の数量
+    + n_train:   学習用データの数量
+    + n_test:    テスト用データの数量
+    ## 出力: なし
+'''
+def makeModelData(datasSize, n_train, n_test):
     # 学習用データ
     l = snapshot_list()
-    n_max = len(l) - 20 - 1001
+    n_max = len(l) - datasSize - n_test -1
 
     timestamp = dt.now(timezone('UTC')).strftime('%Y%m%d%H%M')
     os.makedirs(f'./model/{timestamp}/train/both', exist_ok=True)
@@ -91,11 +274,19 @@ def makeModelData(quantity):
             w = csv.writer(file)
             w.writerow([n, path, should_buy, should_sell])
 
-
-if __name__ == '__main__':
+'''
+    # データから学習用データ・テスト用データを作る
+    ## 入力
+    + datasSise: 1つのデータに用いる気配値の数量
+    + n_train:   学習用データの数量
+    + n_test:    テスト用データの数量
+    ## 出力: なし
+'''
+def createModel(datasSize, n_train, n_test):
+    future_sample = 1
     # 学習用データ
     l = snapshot_list()
-    n_max = len(l) - 20 - 1001
+    n_max = len(l) - datasSize - future_sample - n_test -1
 
     timestamp = dt.now(timezone('UTC')).strftime('%Y%m%d%H%M')
     os.makedirs(f'./model/{timestamp}/train/both', exist_ok=True)
@@ -107,11 +298,12 @@ if __name__ == '__main__':
     for n in range(0,n_max):
         print(n)
 
-        s = snapshots_10(l, n)
+        # s = snapshots_10(l, n)
+        s = snapshots_between(l, n, n+datasSize)
         t = snapshots_traj(s)
 
-        s_current = nth_snapshot(l, n + 10)     # 現在の気配値
-        s_future  = nth_snapshot(l, n + 10 +10) # 10サンプル後の気配値
+        s_current = nth_snapshot(l, n + datasSize)     # 現在の気配値
+        s_future  = nth_snapshot(l, n + datasSize + future_sample) # 1サンプル後の気配値
 
         should_buy  = s_current[300][0] - s_future[301][0] < 0
         should_sell = s_current[301][0] - s_future[300][0] > 0
@@ -137,8 +329,8 @@ if __name__ == '__main__':
 
     # テスト用データ
     l = snapshot_list()
-    n_min = len(l) - 20 - 1000
-    n_max = len(l) - 20
+    n_min = len(l) - datasSize - future_sample - n_train
+    n_max = len(l) - datasSize - future_sample
 
     os.makedirs(f'./model/{timestamp}/test/both', exist_ok=True)
     os.makedirs(f'./model/{timestamp}/test/neither', exist_ok=True)
@@ -149,11 +341,12 @@ if __name__ == '__main__':
     for n in range(n_min,n_max):
         print(n)
 
-        s = snapshots_10(l, n)
+        # s = snapshots_10(l, n)
+        s = snapshots_between(l, n, n+datasSize)
         t = snapshots_traj(s)
 
-        s_current = nth_snapshot(l, n + 10)     # 現在の気配値
-        s_future  = nth_snapshot(l, n + 10 +10) # 10サンプル後の気配値
+        s_current = nth_snapshot(l, n + datasSize)     # 現在の気配値
+        s_future  = nth_snapshot(l, n + datasSize +future_sample) # 1サンプル後の気配値
 
         should_buy  = s_current[300][0] - s_future[301][0] < 0
         should_sell = s_current[301][0] - s_future[300][0] > 0
@@ -176,3 +369,30 @@ if __name__ == '__main__':
         with open(f'./model/{timestamp}/test/reference.csv', 'a') as file:
             w = csv.writer(file)
             w.writerow([n, path, should_buy, should_sell])
+
+if __name__ == '__main__':
+    createModel(20, 3000, 1000)
+
+    # modelArr('201811011301')
+    # loadModel('201811011301',20)
+
+    # datasArr_test, y_test, datasArr_train, y_train = loadDatas('201811011301')
+    # np.save('raw_test',datasArr_test)
+    # np.save('raw_train',datasArr_train)
+    # np.save('y_test',y_test)
+    # np.save('y_train',y_train)
+
+    # datasArr_test = np.load('raw_test.npy')
+    # datasArr_train = np.load('raw_train.npy')
+    # # print(datasArr_test.shape)
+    #
+    # x_test  = []
+    # x_train = []
+    # for datasArr in datasArr_test:
+    #     x_test.append(v.snapshots2Gray(datasArr))
+    #
+    # for datasArr in datasArr_train:
+    #     x_train.append(v.snapshots2Gray(datasArr))
+    #
+    # np.save('x_test',x_test)
+    # np.save('x_train',x_train)
--- visualizer.py
+++ visualizer.py
@@ -125,11 +125,24 @@ def reshape(x,min,max):
         y = x
     return y
 
-# TODO: 気配値の曲面をグレースケールの画像に変換する
+def change0within(x,min,max):
+    y = 0
+    if (x <= max) & (x >= min):
+        y = 0
+    elif x < min:
+        y = x
+    elif x > max:
+        y = x
+    else:
+        y = x
+    return y
+
+# 気配値の曲面をグレースケールの画像に変換する
+## 現在の気配値の最大を基準価格として，基準価格以上の価格を切り捨て
 def snapshots2Gray(snapshots):
     s = snapshots.shape
     # TODO: 全ての値から現在のmidpriceで減じる
-    # print(s[1])
+    # print(s)
     currentBestBid = snapshots[s[0]-1,s[1]//2-1,0]
     currentBestAsk = snapshots[s[0]-1,s[1]//2,0]
     currentMidPrice = (currentBestBid + currentBestAsk )/2
@@ -145,6 +158,226 @@ def snapshots2Gray(snapshots):
         refPrice = currentWorstAsk
 
     # refPrice = currentMidPrice
+
+
+    # TODO: 全ての値を基準価格*2で割る．
+    snapshots[:,:,0] /= (refPrice * 2)
+    # print(snapshots)
+
+    # TODO: [-0.5,0.5]外の価格は+-0.5とする
+    reshaped = snapshots
+    # reshaped[:,:,0] = np.frompyfunc(reshape, 3, 1)(snapshots[:,:,0],-0.5,0.5)
+    # print(reshaped)
+
+    # TODO: 全ての値を0.5加算する
+    reshaped[:,:,0] += 0.5
+
+
+    # TODO: [0,1]をn分割して，時間ごとのヒストグラムを生成する
+    n = s[0]
+    nSeq = s[1]
+    nTime= s[0]
+    arr = np.arange(0,1+1/n,1/n)
+    histSequence = np.zeros((nTime,n))
+    for i in range(nTime): # ある時刻で
+        snapshot = reshaped[i,:,:]
+        # print(snapshot)
+        for j in range(nSeq):
+            for k in range(n):
+                # print(snapshot[j,0])
+                # print(arr[k+1])
+                if (snapshot[j,0] <= arr[k+1]) &( snapshot[j,0] >= arr[k]):
+                    histSequence[i,k] += snapshot[j,1]
+                    break
+                else:
+                    pass
+    # plt.imshow(histSequence, 'gray')
+    # plt.show()
+
+    # histSequence[:,:] = np.frompyfunc(reshape, 3, 1)(histSequence[:,:],0.0,1.0)
+
+    for i in range(nTime):
+        m = mean(histSequence[i,:])
+        med = median(histSequence[i,:])
+        sd = stdev(histSequence[i,:])
+        histSequence[i,:] = np.frompyfunc(reshape, 3, 1)(histSequence[i,:],med-sd,med+sd)
+        s = histSequence[i,:].sum()
+        histSequence[i,:] /= s
+        # print(med-sd,med+sd)
+
+    # histSequence[:,0] = np.frompyfunc(reshape, 3, 1)(histSequence[:,0],0.0,1.0)
+    # histSequence[:,n-1] = np.frompyfunc(reshape, 3, 1)(histSequence[:,n-1],0.0,1.0)
+    # print(histSequence.T)
+    return histSequence.T
+
+
+# 気配値の曲面をグレースケールの画像に変換する
+## 最新の気配値の標準偏差を基準価格として，1σ以上の値を切り捨て
+def snapshots2Gray2(snapshots):
+    s = snapshots.shape
+    # TODO: 全ての値から現在のmidpriceで減じる
+    # print(s)
+    currentBestBid = snapshots[s[0]-1,s[1]//2-1,0]
+    currentBestAsk = snapshots[s[0]-1,s[1]//2,0]
+    currentMidPrice = (currentBestBid + currentBestAsk )/2
+    snapshots[:,:,0] -= currentMidPrice
+    # print(snapshots)
+
+    # TODO: 最新の気配値の標準偏差を基準価格とする
+    sd = stdev(snapshots[s[0]-1,:,0])
+    refPrice = sd
+
+    # TODO: 全ての値を基準価格で割る．
+    snapshots[:,:,0] /= (refPrice)
+    # print(snapshots)
+
+    # TODO: [-0.5,0.5]外の価格は+-0.5とする
+    reshaped = snapshots
+    # reshaped[:,:,0] = np.frompyfunc(reshape, 3, 1)(snapshots[:,:,0],-0.5,0.5)
+    # print(reshaped)
+
+    # TODO: 全ての値を0.5加算する
+    reshaped[:,:,0] += 0.5
+
+
+    # TODO: [0,1]をn分割して，時間ごとのヒストグラムを生成する
+    n = s[0]
+    nSeq = s[1]
+    nTime= s[0]
+    arr = np.arange(0,1+1/n,1/n)
+    histSequence = np.zeros((nTime,n))
+    for i in range(nTime): # ある時刻で
+        snapshot = reshaped[i,:,:]
+        # print(snapshot)
+        for j in range(nSeq):
+            for k in range(n):
+                # print(snapshot[j,0])
+                # print(arr[k+1])
+                if (snapshot[j,0] <= arr[k+1]) &( snapshot[j,0] >= arr[k]):
+                    histSequence[i,k] += snapshot[j,1]
+                    break
+                else:
+                    pass
+    # plt.imshow(histSequence, 'gray')
+    # plt.show()
+
+    # histSequence[:,:] = np.frompyfunc(reshape, 3, 1)(histSequence[:,:],0.0,1.0)
+
+    for i in range(nTime):
+        m = mean(histSequence[i,:])
+        med = median(histSequence[i,:])
+        sd = stdev(histSequence[i,:])
+        histSequence[i,:] = np.frompyfunc(reshape, 3, 1)(histSequence[i,:],med-sd,med+sd)
+        s = histSequence[i,:].sum()
+        histSequence[i,:] /= s
+        # print(med-sd,med+sd)
+
+    # histSequence[:,0] = np.frompyfunc(reshape, 3, 1)(histSequence[:,0],0.0,1.0)
+    # histSequence[:,n-1] = np.frompyfunc(reshape, 3, 1)(histSequence[:,n-1],0.0,1.0)
+    # print(histSequence.T)
+    return histSequence.T
+
+# 気配値の曲面をグレースケールの画像に変換する
+## 最新の気配値の標準偏差を基準価格として，1σ以上の値を切り捨て
+def snapshots2Gray3(snapshots):
+    s = snapshots.shape
+    # TODO: 全ての値から現在のmidpriceで減じる
+    # print(s)
+    currentBestBid = snapshots[s[0]-1,s[1]//2-1,0]
+    currentBestAsk = snapshots[s[0]-1,s[1]//2,0]
+    currentMidPrice = (currentBestBid + currentBestAsk )/2
+    snapshots[:,:,0] -= currentMidPrice
+    # print(snapshots)
+
+    # # TODO: 現在の気配値の値の絶対値の最大値を基準価格とする
+    # currentWorstBid = snapshots[s[0]-1,0,0]
+    # currentWorstAsk = snapshots[s[0]-1,s[1]-1,0]
+    # if (abs(currentWorstBid) > abs(currentWorstAsk)):
+    #     refPrice = currentWorstBid
+    # else:
+    #     refPrice = currentWorstAsk
+
+
+
+    # TODO: 現在の気配値の標準偏差を基準価格とする
+    print(snapshots[s[0]-1,:,0])
+    refPrice = stdev(snapshots[s[0]-1,:,0])
+
+    # TODO: 全ての値を基準価格*2で割る．
+    snapshots[:,:,0] /= (refPrice * 2)
+    # print(snapshots)
+
+    # TODO: [-0.5,0.5]外の価格は+-0.5とする
+    reshaped = snapshots
+    # reshaped[:,:,0] = np.frompyfunc(reshape, 3, 1)(snapshots[:,:,0],-0.5,0.5)
+    # print(reshaped)
+
+    # TODO: 全ての値を0.5加算する
+    reshaped[:,:,0] += 0.5
+
+
+    # TODO: [0,1]をn分割して，時間ごとのヒストグラムを生成する
+    n = s[0]
+    nSeq = s[1]
+    nTime= s[0]
+    arr = np.arange(0,1+1/n,1/n)
+    histSequence = np.zeros((nTime,n))
+    for i in range(nTime): # ある時刻で
+        snapshot = reshaped[i,:,:]
+        # print(snapshot)
+        for j in range(nSeq):
+            for k in range(n):
+                # print(snapshot[j,0])
+                # print(arr[k+1])
+                if (snapshot[j,0] <= arr[k+1]) &( snapshot[j,0] >= arr[k]):
+                    histSequence[i,k] += snapshot[j,1]
+                    break
+                else:
+                    pass
+    # plt.imshow(histSequence, 'gray')
+    # plt.show()
+
+    # histSequence[:,:] = np.frompyfunc(reshape, 3, 1)(histSequence[:,:],0.0,1.0)
+
+    # for i in range(nTime):
+    #     m = mean(histSequence[i,:])
+    #     med = median(histSequence[i,:])
+    #     sd = stdev(histSequence[i,:])
+    #     histSequence[i,:] = np.frompyfunc(reshape, 3, 1)(histSequence[i,:],med-sd,med+sd)
+    #     s = histSequence[i,:].sum()
+    #     histSequence[i,:] /= s
+    #     # print(med-sd,med+sd)
+
+    for i in range(nTime):
+        m = mean(histSequence[i,:])
+        med = median(histSequence[i,:])
+        sd = stdev(histSequence[i,:])
+        histSequence[i,:] = np.frompyfunc(change0within, 3, 1)(histSequence[i,:],med-sd*2,med+sd*2)
+        s = histSequence[i,:].sum()
+        if s != 0:
+            histSequence[i,:] /= s
+        # print(med-sd,med+sd)
+
+    # histSequence[:,0] = np.frompyfunc(reshape, 3, 1)(histSequence[:,0],0.0,1.0)
+    # histSequence[:,n-1] = np.frompyfunc(reshape, 3, 1)(histSequence[:,n-1],0.0,1.0)
+    # print(histSequence.T)
+    return histSequence.T
+
+# 気配値の曲面をグレースケールの画像に変換する
+## 現在の気配値の最大を基準価格として，基準価格以上の価格を切り捨て
+def snapshots2Gray4(snapshots):
+    s = snapshots.shape
+    # TODO: 全ての値から現在のmidpriceで減じる
+    # print(s)
+    currentBestBid = snapshots[s[0]-1,s[1]//2-1,0]
+    currentBestAsk = snapshots[s[0]-1,s[1]//2,0]
+    currentMidPrice = (currentBestBid + currentBestAsk )/2
+    snapshots[:,:,0] -= currentMidPrice
+    # print(snapshots)
+
+    # TODO: 現在の気配値の標準偏差を基準価格とする
+    print(snapshots[s[0]-1,:,0])
+    refPrice = stdev(snapshots[s[0]-1,:,0])
 
     # TODO: 全ての値を基準価格*2で割る．
     snapshots[:,:,0] /= (refPrice * 2)
@@ -230,24 +463,25 @@ if __name__ == '__main__':
     # timestamps = getRecentTimestamps(100)
     # outputSeriesOf(snapshots, getTimestampDiff(timestamps))
 
-    # 特定期間の気配値の取得
+    # # 特定期間の気配値の取得
     # l = getSnapshotsSize()
     # # print(getSnapshotsSize())
     # first = l - 300
     # final = l - 200
     # snapshots = getSnapshotsBetween(first,final)
-
-    # 気配値をグレースケールに変換
-    # ssGray = snapshots2Gray(snapshots)
+    #
+    # # 気配値をグレースケールに変換
+    # ssGray = snapshots2Gray2(snapshots)
     # print(ssGray)
     # plt.imshow(ssGray, 'gray')
     # plt.show()
 
-    # グラフを動画にして出力
+    # n個の気配値からグラフを動画にして出力
+    # n = 100
     # fig = plt.figure()
     # ims = []
     # l = getSnapshotsSize()
-    # for i in np.arange(l-2,l-1):
+    # for i in np.arange(l-n-1000,l-1000):
     #     print(f'{i} in {l} snapshots')
     #     snapshots = getSnapshotsBetween(i-100,i)
     #     ssGray = snapshots2Gray(snapshots)
@@ -257,7 +491,12 @@ if __name__ == '__main__':
     # ani = animation.ArtistAnimation(fig, ims, interval=100)
     # ani.save('anim.mp4', writer="ffmpeg")
 
-    snapshots = getSnapshotsBetween(4000,4100)
-    ssGray = snapshots2Gray(snapshots)
-    im = plt.imshow(ssGray, 'gray')
+    # snapshots = getSnapshotsBetween(1500,1600)
+    # ssGray1 = snapshots2Gray(snapshots)
+    # im = plt.imshow(ssGray1, 'gray')
+    # plt.show()
+
+    snapshots = getSnapshotsBetween(1500,1600)
+    ssGray3 = snapshots2Gray3(snapshots)
+    im2 = plt.imshow(ssGray3, 'gray')
     plt.show()
